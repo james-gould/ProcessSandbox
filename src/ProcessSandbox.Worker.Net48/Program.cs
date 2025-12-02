1f7bd47e60c8718a7b23b9f6e013943f946b4613
Jonny Muir <jonnymoo@hotmail.com>
2025-12-02 08:41:54 +0000

    Add Program.cs for .NET Framework 4.8 application

---
+++ src/ProcessSandbox.Worker.Net48/Program.cs
@@ -0,0 +1,16 @@
+namespace ProcessSandbox.Worker.Net48;
+
+
+using System;
+
+/// <summary>
+/// 
+/// </summary>
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Entry point for your application
+        Console.WriteLine("Hello from .NET Framework 4.8!");
+    }
+}
